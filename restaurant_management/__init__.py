839d96d4831afaec379e7c9736b91fd71599939e
ajaymarri1808-art <ajaymarri1808@gmail.com>
2025-08-12 20:00:26 +0530

    28366

---
+++ restaurant_management/__init__.py
@@ -0,0 +1,43 @@
+body{
+    font-family:Arial, Helvetica, sans-serif;
+    background-color: #f4f4f4;
+    margin:0;
+    padding: 0;
+}
+header{
+    background-color: #4CAF50;
+    color: white;
+    padding: 15px 0;
+    text-align: center;
+    font-size: 1.5rem;
+}
+
+h1, h2, h3 {
+    color: #333;
+}
+
+.container{
+    width: 80%;
+    margin: 20px auto;
+}
+
+section{
+    background: white;
+    padding: 15px;
+    margin-bottom; 20px;
+    border-radius: 8px;
+    box-shadow:0 2px 5px rgba(0,0,0,0.1);
+}
+
+footer{
+    text-align:center;
+    padding:15px;
+    background-color: #333;
+    color: white;
+    margin-top: 40px;
+}
+
+a{
+    color: #4CAF50;
+    text-decoration: none;
+}
